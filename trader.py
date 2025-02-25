79ae6a7deed8ab741ba6fa582c44894bcacf7534
Austin Jiang <austinjiang@Austins-MacBook-Pro.local>
2025-02-25 16:28:25 +0800

    first commit

---
+++ trader.py
@@ -0,0 +1,24 @@
+from strategies import TestStrategy
+import backtrader as bt
+import datetime
+# from strategies import TestStrategy
+
+cerebro = bt.Cerebro()
+cerebro.broker.set_cash(1000000)
+
+# path_data = 'orcl-1995-2014.csv'
+
+# Create a Data Feed
+data = bt.feeds.YahooFinanceCSVData(
+    dataname='orcl-1995-2014.csv',
+    # Do not pass values before this date
+    fromdate=datetime.datetime(2000, 1, 1),
+    # Do not pass values after this date
+    todate=datetime.datetime(2000, 12, 31),
+    reverse=False)
+cerebro.adddata(data)
+
+# cerebro.adddata(TestStratge)
+print('Starting Portfolio Value: %.2f' %cerebro.broker.getvalue())
+cerebro.run()
+print('Final Portfolio Value: %.2f' %cerebro.broker.getvalue())
